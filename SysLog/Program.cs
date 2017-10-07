f8f0427b1b40475597f8f00b4d97c96db73892dc
Alexander Medchenko <djblend@live.ru>
2017-10-07 16:27:37 +0300

    "Причесал" код

--- SysLog/Program.cs
+++ SysLog/Program.cs
@@ -5,11 +5,11 @@ using System.Text.RegularExpressions;
 
 namespace SysLog
 {
-    class Program
+    internal class Program
     {
         // Variables
-        private static string InputFile = @"D:\Downloads\syslog.log";
-        private static string OutputFile = @"D:\result.json";
+        private const string InputFile = @"D:\Downloads\syslog.log";
+        private const string OutputFile = @"D:\result.json";
         private static int _counter;
 
         // Patterns
@@ -31,80 +31,75 @@ namespace SysLog
         private static readonly Regex ParseTargetIp = new Regex(TargetIpRegEx);
 
         // Main program
-        static void Main()
+        private static void Main()
         {
-            // 1
-            // Declare new List.
-            List<string> lines = new List<string>();
+            // Declare new List
+            var lines = new List<string>();
 
-            // 2
-            // Use using StreamReader for disposing.
-            using (StreamReader r = new StreamReader(InputFile))
+            // Use using StreamReader for disposing
+            using (var r = new StreamReader(InputFile))
             {
-                // 3
-                // Use while != null pattern for loop.
+                // Use while != null pattern for loop
                 string line;
                 while ((line = r.ReadLine()) != null)
                 {
-                    // 4
-                    // Insert logic here.
-                    // ...
-                    // The "line" value is a line in the file.
-                    // Add it to our List.
+                    // The "line" value is a line in the file
+                    // Add it to our List
                     lines.Add(line);
                 }
             }
 
-            // 5
             // Use RegEx to compare all the lines and write in output file.
             while (_counter != lines.Count)
             {
-                foreach (string s in lines)
+                foreach (var s in lines)
                 {
-                    //// Read each line until EOF.
                     // Try to match each line against the Regex.
-                    Match m1 = ParseDateTime.Match(s);
+
                     // DateTime
+                    var m1 = ParseDateTime.Match(s);
                     if (m1.Success)
                     {
                         File.AppendAllText(OutputFile, @"{" + Environment.NewLine + "DateTime: " + m1 + "," + Environment.NewLine);
                     }
                     // IP
-                    Match m2 = ParseIp.Match(s);
+                    var m2 = ParseIp.Match(s);
                     if (m2.Success)
                     {
                         File.AppendAllText(OutputFile, @"IP: " + m2 + "," + Environment.NewLine);
                     }
                     // Source
-                    Match m3 = ParseSource.Match(s);
+                    var m3 = ParseSource.Match(s);
                     if (m3.Success)
                     {
                         File.AppendAllText(OutputFile, @"Source: " + m3 + "," + Environment.NewLine);
                     }
                     // Header
-                    Match m4 = ParseHeader.Match(s);
+                    var m4 = ParseHeader.Match(s);
                     if (m4.Success)
                     {
                         File.AppendAllText(OutputFile, @"Header: " + m4.ToString().Replace(":", "") + "," + Environment.NewLine);
                     }
                     // Community
-                    Match m5 = ParseCommunity.Match(s);
+                    var m5 = ParseCommunity.Match(s);
                     if (m5.Success)
                     {
                         File.AppendAllText(OutputFile, @"Community: " + m5 + "," + Environment.NewLine);
                     }
                     // RawMessage
-                    Match m6 = ParseRawMessage.Match(s);
+                    var m6 = ParseRawMessage.Match(s);
                     if (m6.Success)
                     {
                         File.AppendAllText(OutputFile, @"RawMessage: " + m6 + "," + Environment.NewLine);
                     }
                     // TargetIp
-                    Match m7 = ParseTargetIp.Match(s);
+                    var m7 = ParseTargetIp.Match(s);
                     if (m7.Success)
                     {
                         File.AppendAllText(OutputFile, @"TargetIp: " + m7 + Environment.NewLine + "}" + Environment.NewLine);
                     }
+
+                    // Increment _counter
                     _counter++;
                 }
             }
